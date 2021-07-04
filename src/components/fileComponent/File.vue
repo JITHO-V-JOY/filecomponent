104bb2885bb03c8d618c085589a6516d32a71a7f
jitho <jithovjoy@gmail.com>
2021-07-05 00:23:09 +0530

    sample dropzones

---
+++ src/components/fileComponent/File.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="dropzone">
+        <input type="file" multiple name="files" />
+        <i class="fas fa-cloud-upload-alt" style="color:gray; font-size:17px;"></i>
+        <div>
+            <span> Drag & Drop or <span class="browse">browse</span></span>
+        </div>             
+    </div>
+       
+</template>
+
+<script>
+export default {
+    props:['label']
+}
+</script>
+
+<style scoped>
+.dropzone{
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    padding: 2px;
+    height: 45px;
+    border: rgb(153, 152, 152) dashed 2px;
+}
+.browse{
+    border: none;
+    color: red;
+    cursor: pointer;
+    text-decoration: underline;
+}
+input[type="file"]{
+        overflow: hidden;
+        width: 100%;
+        height: 40px;
+        opacity: 0;
+        position: absolute;
+        cursor: pointer;
+        
+}
+
+
+</style>
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,10 +1,27 @@
 <template>
-    <div class="container-fluid ">
+    <div class="container" style="margin-top:10px;">
+        <div class="row">
+            <div class="col-12 col-md-2">
+                <File  />
+            </div>
+              <div class="col-12 col-md-4">
+                <File  />
+            </div>
+              <div class="col-12 col-md-4">
+                <File  />
+            </div>
+            
+           
+        </div>
     </div>
 </template>
 <script>
+import File from '../fileComponent/File.vue'
 export default {
-    name:'Home'
+    name:'Home',
+    components:{
+        File
+    }
 }
 </script>
 
