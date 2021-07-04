18b3a1b844b665ddcee2de8d33c5692e51f8cc6f
jitho <jithovjoy@gmail.com>
2021-07-04 20:13:04 +0530

    added bootstrap-vue and created header

---
+++ src/components/header/Header.vue
@@ -0,0 +1,23 @@
+<template>
+        <div>
+  <b-navbar type="dark" variant="danger">
+    <b-navbar-nav>
+      <b-nav-item  class="logo" href="#">File component</b-nav-item>
+
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+   name:'Header'
+}
+</script>
+
+<style scoped>
+    .logo{
+        color:white;
+        font-size: 25px;
+    }
+</style>
